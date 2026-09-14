e54ae1e7ab9f42d359f2785e6ce46336ae43b74b
bruno cabral <bruno88cabral@gmail.com>
2026-09-14 11:29:36 -0300

    adiciona clean dos mocks

--- src/exercicios/BDD/cadastro.test.ts
+++ src/exercicios/BDD/cadastro.test.ts
@@ -8,26 +8,31 @@ type EnderecoMock = {
   uf: string
 }
 
-function createMock(responseMock: EnderecoMock) {
+function createResolveMock(responseMock: EnderecoMock) {
     vi.stubGlobal('fetch', vi.fn().mockResolvedValue({
         ok: true,
         json: async () => responseMock
     }))
 }
 
+
+
 describe('function -> cadastrarCliente', () => {
 
+    afterEach(() => {
+        vi.unstubAllGlobals()
+        vi.restoreAllMocks()
+    })
 
     it('deve retornar endereço valido', async () => {
-      
         // Arrange
-       createMock({
-                cep: '11111-221',
-                logradouro: 'Rua da paz',
-                bairro: 'Bairro de cima',
-                localidade: 'Porto Alegre',
-                uf: 'RS'
-            })
+        createResolveMock({
+            cep: '11111-221',
+            logradouro: 'Rua da paz',
+            bairro: 'Bairro de cima',
+            localidade: 'Porto Alegre',
+            uf: 'RS'
+        })
       
 
         const cliente: NovoCliente = {
