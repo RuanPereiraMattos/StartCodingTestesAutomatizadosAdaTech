5224793dfa72f3656bb98ebdc1eecb8ce2b397e2
RuanPereiraMattos <ruandupg@gmail.com>
2026-09-16 14:08:19 -0300

    test: RED - aplica a faixa correta nos limites (4x, 5x, 8x, 9x, 12x, 13x)

--- src/exercicios/TDD/parcelamento.test.ts
+++ src/exercicios/TDD/parcelamento.test.ts
@@ -34,7 +34,24 @@ describe('calcularParcelamento', () => {
         })
         it('aplica 8% sobre o total quando for de 9x a 12x')
         it('aplica 10% sobre o total quando for de 13x a 18x')
-        it('aplica a faixa correta nos limites (4x, 5x, 8x, 9x, 12x, 13x)')
+        it('aplica a faixa correta nos limites (4x, 5x, 8x, 9x, 12x, 13x)', () => {
+            const valorCompra = 1000;
+            const numeroParcelas: {numeroParcela:number, valorParcela: number}[] = [
+                { numeroParcela: 4,  valorParcela: 250 },
+                { numeroParcela: 5,  valorParcela: 210 },
+                { numeroParcela: 8,  valorParcela: 131.25 },
+                { numeroParcela: 9,  valorParcela: 120 },
+                { numeroParcela: 12, valorParcela: 90 },
+                { numeroParcela: 13, valorParcela: 84.61538461538461 }
+            ];
+            numeroParcelas.forEach(({ numeroParcela, valorParcela }) => {
+                const resultado: ResultadoParcelamento = calcularParcelamento(valorCompra, numeroParcela);
+                expect(resultado).toEqual({
+                    valorParcela,
+                    totalParcelas: numeroParcela
+                });
+            });
+        })
     })
     describe('arredondamento', () => {
         it('arredonda o valor da parcela para 2 casas decimais')
