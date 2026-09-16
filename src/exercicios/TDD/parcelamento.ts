6655c23ccc6aa5a5f4bff39531af4cd8ff46e2b3
RuanPereiraMattos <ruandupg@gmail.com>
2026-09-16 13:35:39 -0300

    feat: GREEN - aplica 5% sobre o total quando for de 5x a 8x

--- src/exercicios/TDD/parcelamento.ts
+++ src/exercicios/TDD/parcelamento.ts
@@ -6,6 +6,14 @@ export function calcularParcelamento(
     valorCompra: number,
     numeroParcelas: number
 ): ResultadoParcelamento {
+    if (numeroParcelas >= 5 && numeroParcelas <= 8){
+        const valorTotalCompra = valorCompra  * 1.05;
+        const result: ResultadoParcelamento = {
+            valorParcela: valorTotalCompra / numeroParcelas,
+            totalParcelas: numeroParcelas
+        }
+        return result;
+    }
     const result: ResultadoParcelamento = {
         valorParcela: valorCompra / numeroParcelas,
         totalParcelas: numeroParcelas
