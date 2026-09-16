a753acaf0deed66a8f5e2022c6cdbe44e02c8950
RuanPereiraMattos <ruandupg@gmail.com>
2026-09-16 13:41:28 -0300

    test: RED - aplica 10% sobre o total quando for de 13x a 18x

--- src/exercicios/TDD/parcelamento.test.ts
+++ src/exercicios/TDD/parcelamento.test.ts
@@ -21,7 +21,7 @@ describe('calcularParcelamento', () => {
                 totalParcelas: numeroParcelas
             });
         });
-    })
+    });
     describe('com juros', () => {
         it('aplica 5% sobre o total quando for de 5x a 8x', () => {
             const valorCompra = 1000;
@@ -31,7 +31,7 @@ describe('calcularParcelamento', () => {
                 valorParcela: 131.25,
                 totalParcelas: numeroParcelas
             });
-        })
+        });
         it('aplica 8% sobre o total quando for de 9x a 12x', () => {
             const valorCompra = 1000;
             const numeroParcelas = 9;
@@ -40,8 +40,16 @@ describe('calcularParcelamento', () => {
                 valorParcela: 120,
                 totalParcelas: numeroParcelas
             });
+        });
+        it('aplica 10% sobre o total quando for de 13x a 18x', () => {
+            const valorCompra = 1000;
+            const numeroParcelas = 13;
+            const resultado: ResultadoParcelamento = calcularParcelamento(valorCompra, numeroParcelas);
+            expect(resultado).toEqual({
+                valorParcela: 84.61,
+                totalParcelas: numeroParcelas
+            });
         })
-        it('aplica 10% sobre o total quando for de 13x a 18x')
         it('aplica a faixa correta nos limites (4x, 5x, 8x, 9x, 12x, 13x)', () => {
             const valorCompra = 1000;
             const numeroParcelas: {numeroParcela:number, valorParcela: number}[] = [
