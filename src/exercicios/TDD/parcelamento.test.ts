aa2ab4547dd9cbac99176e7149c78097ea27d922
RuanPereiraMattos <ruandupg@gmail.com>
2026-09-16 14:40:50 -0300

    test: RED - lanca erro quando o numero de parcelas for maior que 18

--- src/exercicios/TDD/parcelamento.test.ts
+++ src/exercicios/TDD/parcelamento.test.ts
@@ -104,7 +104,11 @@ describe('calcularParcelamento', () => {
             const numeroParcelas = 0;
             expect(() => calcularParcelamento(valorCompra, numeroParcelas)).toThrow("O número de parcelas não pode ser menor que 1");
         });
-        it('lança erro quando o número de parcelas for maior que 18')
+        it('lança erro quando o número de parcelas for maior que 18', () => {
+            const valorCompra = 1000;
+            const numeroParcelas = 19;
+            expect(() => calcularParcelamento(valorCompra, numeroParcelas)).toThrow("O número de parcelas não pode ser maior que 18");
+        });
         it('lança erro quando o número de parcelas não for inteiro')
         it('lança erro quando o valor da compra for zero ou negativo')
     })
