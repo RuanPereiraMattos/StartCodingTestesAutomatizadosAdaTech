14010616aab86683519557a4883840561374991d
RuanPereiraMattos <ruandupg@gmail.com>
2026-09-18 11:38:49 -0300

    test: GREEN - adicionarItem retorna novo carrinho com item adicionado

--- src/exercicios/BDD/carrinho.test.ts
+++ src/exercicios/BDD/carrinho.test.ts
@@ -1,11 +1,22 @@
 import { describe, it, expect } from "vitest";
-import { Carrinho, criarCarrinho } from "./carrinho";
+import { adicionarItem, Carrinho, criarCarrinho, Item } from "./carrinho";
 
 describe("Carrinho", () => {
     
     it("criarCarrinho()", () => {
         const carrinho: Carrinho = criarCarrinho();
         expect(carrinho).toEqual({ itens: [] });
+    });
+
+    it("adicionarItem()", () => {
+        const carrinho: Carrinho = { itens: [] };
+        const item: Item = {
+            nome: "Sabão em Pó",
+            precoEmCentavos: 2500,
+            quantidade: 1
+        };
+        const novoCarrinho = adicionarItem(carrinho, item);
+        expect(novoCarrinho).toEqual({ itens: [item] });
     });
 
 });
